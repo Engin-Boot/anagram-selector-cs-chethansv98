d8537ef21210f3c415e4d5533f7f256d7882fbd8
Chethan S V <Chethan.SV@philips.com>
2020-09-07 15:54:34 +0530

    update Anagram.cs

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -6,11 +6,25 @@ namespace Anagram
     public class AnagramSelector
     {
         public bool WordPairIsAnagram(string word1, string word2) {
-            //Insert the correct implementation here
+            char[] char1 = word1.ToLower().ToCharArray();  
+            char[] char2 = word2.ToLower().ToCharArray();  
+            Array.Sort(char1);  
+            Array.Sort(char2); 
+            string NewWord1 = new string(char1);  
+            string NewWord2 = new string(char2); 
+            if (NewWord1 == NewWord2) {
+                return true;
+            }
             return false;
         }
         public List<string> SelectAnagrams(string word, List<string> candidates) {
-            //Insert the correct implementation here
+            for(int i = 0; i < candidates.Count; i++)
+            {
+                if(!WordPairIsAnagram(word, candidates[i]))
+                {
+                    candidates.RemoveAt(i);
+                }
+            }
             return candidates;
         }
     }
